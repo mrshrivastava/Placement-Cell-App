c00938f1eb92f76ac39a0a122d3559461f9244e7
Int-ON <intonanalytics@gmail.com>
2023-06-05 16:53:01 +0530

    upload page added for stats

---
+++ app/src/main/java/com/example/k_g/stats_upload_page.java
@@ -0,0 +1,14 @@
+package com.example.k_g;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stats_upload_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats_upload_page);
+    }
+}
